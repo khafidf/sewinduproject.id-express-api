ce974cfa616cb903dd1dd7159bbb3fc4e36cd24e
khafidf <khfd027@gmail.com>
2023-09-27 12:47:05 +0700

    Booking model created

---
+++ models/bookingModel.js
@@ -0,0 +1,35 @@
+import { Schema, Types, model } from "mongoose";
+
+const Booking = new Schema(
+	{
+		userId: {
+			type: Types.ObjectId,
+			required: true,
+			ref: "user",
+		},
+		packageId: {
+			type: Types.ObjectId,
+			required: true,
+			ref: "package",
+		},
+		date: [
+			{
+				type: Date,
+				required: true,
+			},
+			{
+				type: Date,
+				default: null,
+			},
+		],
+		status: {
+			type: Number,
+			default: 0,
+		},
+	},
+	{
+		versionKey: false,
+	}
+);
+
+export default model("booking", Booking);
