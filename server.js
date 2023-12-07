aadb9e5a6df090e9a44da94972765caa6359545f
khafidf <khfd027@gmail.com>
2023-12-07 16:35:58 +0700

    add cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(compression());
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "https://sparkling-trifle-08bace.netlify.app/",
 		credentials: true,
 	})
 );
